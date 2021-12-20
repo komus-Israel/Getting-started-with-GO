e88a4cd3f5c13b2a147cdd4da89a430f3ff9e213
komus-Israel <komolehinIsrael@gmail.com>
2021-12-20 10:40:13 +0100

    assigning values to variables

--- tutorial.go
+++ tutorial.go
@@ -1,8 +1,18 @@
-package main
+package main	
+
+//	go raises an exception if package main isnt found. It doen't have to be main but the package name must be the name
+//	the functin that will serve as an entry point for your applicatioin 
+
+
 
 import "fmt"
 
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("what is your name")
+	
+	name := "timi"
+	age := 3.8
+
+	fmt.Println(name, age)
 }
